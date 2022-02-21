7975db2be0266e2319a81f79b6a2b4bae8ae6b27
robastel <rob.astel@gmail.com>
2022-02-21 16:50:04 -0700

    Use all supported_resource_types by default (#19)
    * Use all supported_resource_types by default
    
    Signed-off-by: Robert Astel <rob.astel@gmail.com>
    
    * Handle mutual exclusivity between resource_type and model arguments
    
    Signed-off-by: Robert Astel <rob.astel@gmail.com>
    
    * Pin MarkupSafe==2.0.1 for dbt-core<1.0.0 due to https://github.com/dbt-labs/dbt-core/issues/4745
    
    Signed-off-by: Robert Astel <rob.astel@gmail.com>

--- dbt_invoke/internal/_utils.py
+++ dbt_invoke/internal/_utils.py
@@ -159,6 +159,9 @@ def dbt_ls(
     default_arguments = list()
     if not any(resource_selection_arguments.values()):
         default_arguments.append(f'--select {ctx.config["project_name"]}')
+    # Use all supported_resource_types unless a resource_type or models
+    # kwarg is given
+    if not kwargs.get('resource_type') and not kwargs.get('models'):
         if supported_resource_types:
             for rt in supported_resource_types:
                 default_arguments.append(f'{get_cli_kwargs(resource_type=rt)}')
